c5799883ff011202fb824ca50ea8d8e490ea65e9
nguyensteven626 <90593359+nguyensteven626@users.noreply.github.com>
2022-02-09 19:33:47 -0800

    bankaccount commit

---
+++ bankaccount.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    
+    def __init__(self, int_rate = .01, balance = 0): 
+        
+        self.int_rate = int_rate
+        self.balance = balance
+        
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        self.balance -= amount
+        if(amount > self.balance):
+            print(f"Insufficient funds: Charging a $5 fee")
+        self.balance -= 5
+        return self 
+    def display_account_info(self):
+        self.balance = balance
+        print(f"Balance: {self.balance}")
+        return self
+        
+    def yield_interest(self):
+        self.balance * self.int_rate
+        if( self.balance > 0)
+        print(self.balance)
+        return self
+  
